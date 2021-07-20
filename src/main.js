b0c0cc08aeb55ba335f07a8ad43f29cf7cabe79a
Vladimir Panov <v.panov@newizze.com>
2021-07-20 21:32:15 +0300

    Настройка вебпака

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import VueRouter from 'vue-router';
 import { router } from '../src/routes/router';
 import store from '../src/store/index';
 import { BootstrapVue } from 'bootstrap-vue';
-import axios from 'axios';
-
+window.axios = require('axios');
+window.axios.defaults.headers = {
+  'Content-Type': 'application/json',
+}
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'element-ui/lib/theme-chalk/index.css';
